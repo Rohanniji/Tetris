9e290e57830b9c97f849c1889bbd5c6e73bc62db
Rohanniji <156123488+Rohanniji@users.noreply.github.com>
2024-02-23 10:26:28 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,176 @@
-
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (Sprite2.isHittingTile(CollisionDirection.Left) || Sprite2.isHittingTile(CollisionDirection.Right)) {
+    	
+    }
+    if (Sprite2.isHittingTile(CollisionDirection.Bottom)) {
+        if (Sprite2.image.equals(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            `)) {
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 2), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 3), assets.tile`myTile3`)
+        } else if (Sprite2.image.equals(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . f f f f f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f 2 2 2 f . . . . . . 
+            . . . . . f f f f f f f f f . . 
+            . . . . . f 2 2 2 f 2 2 2 f . . 
+            . . . . . f 2 2 2 f 2 2 2 f . . 
+            . . . . . f 2 2 2 f 2 2 2 f . . 
+            . . . . . f f f f f f f f f . . 
+            `)) {
+            tiles.setTileAt(tiles.getTileLocation(location.column + 1, location.row - 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 2), assets.tile`myTile3`)
+        } else {
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 2), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column, location.row - 3), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(location.column - 1, location.row - 3), assets.tile`myTile3`)
+        }
+        for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
+            tiles.setWallAt(tiles.getTileLocation(value.column, value.row), true)
+        }
+        sprites.destroy(Sprite2)
+        SpawnSprite()
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`myTile1`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    SpawnSprite()
+})
+function SpawnSprite () {
+    if (NextSprite.image.equals(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)) {
+        Sprite2 = sprites.create(Blocks._pickRandom(), SpriteKind.Player)
+        Sprite2.scale += 2.5
+    } else {
+        Sprite2 = sprites.create(NextSprite.image, SpriteKind.Player)
+    }
+    NextSprite.setImage(Blocks._pickRandom())
+    tiles.placeOnTile(Sprite2, tiles.getTileLocation(randint(2, 12), 2))
+    Sprite2.vy = 75
+    controller.moveSprite(Sprite2)
+}
+let Sprite2: Sprite = null
+let NextSprite: Sprite = null
+let Blocks: Image[] = []
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 515
+    export const ARCADE_SCREEN_HEIGHT = 515
+}
+tiles.setCurrentTilemap(tilemap`level2`)
+Blocks = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f 2 2 2 f . . . . . . 
+    . . . . . f f f f f f f f f . . 
+    . . . . . f 2 2 2 f 2 2 2 f . . 
+    . . . . . f 2 2 2 f 2 2 2 f . . 
+    . . . . . f 2 2 2 f 2 2 2 f . . 
+    . . . . . f f f f f f f f f . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . f 2 2 2 f 2 2 2 f . . . . . 
+    . . f 2 2 2 f 2 2 2 f . . . . . 
+    . . f 2 2 2 f 2 2 2 f . . . . . 
+    . . f f f f f f f f f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f 2 2 2 f . . . . . 
+    . . . . . . f f f f f . . . . . 
+    `]
+NextSprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(NextSprite, tiles.getTileLocation(23, 13))
+NextSprite.scale += 8
+info.setScore(0)
