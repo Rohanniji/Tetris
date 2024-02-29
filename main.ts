2706049d5f51815d737a3aac160ac414f707f68a
Rohanniji <156123488+Rohanniji@users.noreply.github.com>
2024-02-29 10:48:10 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    Rotate(RotationsLshape)
+})
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     if (sprite.isHittingTile(CollisionDirection.Bottom)) {
         if (sprite.image.equals(img`
@@ -104,10 +107,10 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
             . . . . . . f 2 2 2 f . . . . . 
             . . . . . . f f f f f . . . . . 
             `)) {
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row - 2), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row - 2), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row - 3), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
         } else if (sprite.image.equals(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -126,10 +129,10 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
             . . f 2 2 2 f 2 2 2 f 2 2 2 f . 
             . . f f f f f f f f f f f f f . 
             `)) {
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row - 2), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
         } else if (sprite.image.equals(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -148,25 +151,22 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
             . . . . f 2 2 2 f 2 2 2 f . . . 
             . . . . f f f f f f f f f . . . 
             `)) {
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
+            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
             tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row - 1), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row - 2), assets.tile`myTile3`)
-            tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row - 3), assets.tile`myTile3`)
         } else {
             tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
             tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row + 1), assets.tile`myTile3`)
             tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column - 1, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
             tiles.setTileAt(tiles.getTileLocation(Sprite2.tilemapLocation().column + 1, Sprite2.tilemapLocation().row), assets.tile`myTile3`)
         }
-        for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
-            tiles.setWallAt(tiles.getTileLocation(value.column, value.row), true)
+        for (let ValueTurq of tiles.getTilesByType(assets.tile`myTile3`)) {
+            tiles.setWallAt(tiles.getTileLocation(ValueTurq.column, ValueTurq.row), true)
         }
         sprites.destroy(sprite)
         SpawnSprite()
     }
-})
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    Rotate(1)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (count < 1) {
@@ -205,16 +205,31 @@ function SpawnSprite () {
     NextSprite.setImage(Blocks._pickRandom())
     tiles.placeOnTile(Sprite2, tiles.getTileLocation(randint(2, 12), 2))
     Sprite2.vy = 75
-    controller.moveSprite(Sprite2, 100, 0)
 }
-function DestroyRow () {
-    tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`myTile1`)
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Sprite2.tileKindAt(TileDirection.Left, assets.tile`myTile0`)) {
+        Sprite2.x += 0
+    }
+    Sprite2.x += -17.5
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Sprite2.tileKindAt(TileDirection.Right, assets.tile`myTile0`)) {
+        Sprite2.x += 0
+    }
+    Sprite2.x += 17.5
+})
+function PointBoost (bool: boolean) {
+    if (bool) {
+        tiles.setTileAt(tiles.getTileLocation(15, 15), assets.tile`myTile0`)
+    } else {
+        tiles.setTileAt(tiles.getTileLocation(15, 15), assets.tile`myTile1`)
+    }
 }
 controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
     Sprite2.vy = 150
     info.changeScoreBy(1)
 })
-function Rotate (num: number) {
+function Rotate (list: Image[]) {
     if (Sprite2.image.equals(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -360,10 +375,16 @@ function Rotate (num: number) {
             . . . . . f f f f f f f f f . . 
             `)
     } else {
-        index = RotationsLshape.indexOf(Sprite2.image)
+        index = list.indexOf(Sprite2.image)
+        Sprite2.setImage(list[(index + 1) % 4])
     }
-    info.changeScoreBy(num)
+    if (tiles.tileAtLocationEquals(tiles.getTileLocation(15, 15), assets.tile`myTile1`)) {
+        info.changeScoreBy(1)
+    }
+    info.changeScoreBy(10)
 }
+let RowBlue = 0
+let RowTurq = 0
 let index = 0
 let Sprite2: Sprite = null
 let count = 0
@@ -594,24 +615,30 @@ tiles.placeOnTile(NextSprite, tiles.getTileLocation(23, 13))
 NextSprite.scale += 8
 info.setScore(0)
 let flag = true
+PointBoost(game.ask("Do you want a point boost?"))
 game.onUpdate(function () {
-    for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
-        if (tiles.tileAtLocationEquals(tiles.getTileLocation(value.column, value.row + 1), assets.tile`myTile3`)) {
+    for (let ValueTurq of tiles.getTilesByType(assets.tile`myTile`)) {
+        if (tiles.tileAtLocationEquals(tiles.getTileLocation(ValueTurq.column, ValueTurq.row + 1), assets.tile`myTile3`)) {
             game.gameOver(false)
         }
     }
     for (let row = 0; row <= 29; row++) {
-        flag = true
         for (let col = 0; col <= 12; col++) {
             if (!(tiles.tileAtLocationEquals(tiles.getTileLocation(col + 1, row + 1), assets.tile`myTile3`))) {
                 flag = false
             }
-        }
-        if (flag) {
-            let col = 0
-            tiles.setTileAt(tiles.getTileLocation(col, row), assets.tile`myTile1`)
-            for (let value of tiles.getTilesByType(assets.tile`myTile3`)) {
-                tiles.setTileAt(tiles.getTileLocation(value.column, value.row + 1), assets.tile`myTile3`)
+            if (flag) {
+                tiles.setTileAt(tiles.getTileLocation(col + 1, row + 1), assets.tile`myTile6`)
+                for (let ValueTurq of tiles.getTilesByType(assets.tile`myTile6`)) {
+                    RowTurq = ValueTurq.row
+                }
+                for (let ValueBlue of tiles.getTilesByType(assets.tile`myTile3`)) {
+                    RowBlue = ValueBlue.row
+                    if (RowBlue < RowTurq) {
+                        tiles.setTileAt(tiles.getTileLocation(ValueBlue.column, ValueBlue.row + 1), assets.tile`myTile3`)
+                    }
+                    tiles.setTileAt(tiles.getTileLocation(ValueBlue.column, ValueBlue.row), assets.tile`myTile3`)
+                }
             }
         }
     }
